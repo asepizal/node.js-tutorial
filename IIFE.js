22d95b77b2d75bf8b8e68b395ca7c832e29b8c1d
asepizal <aseprizal2496@gmail.com>
2023-05-23 10:38:48 +0700

    sedang belajar buffer

---
+++ IIFE.js
@@ -0,0 +1,12 @@
+(function(){
+    const superHero = "Batman"
+    console.log("Batman")
+})();
+
+(function(){
+    const superHero = "Superman"
+    console.log("Superman");
+})();
+
+// jalankan di terminal file ini
+// node.js melakukan hal serupa ketika kita mengeksekusi setiap module
---
+++ main.mjs
@@ -0,0 +1,18 @@
+// import 1 data
+// import add from './math.mjs'
+
+// console.log(add(1,3))
+
+// import 2 data atau lebih dengan export default
+// import math from './math.mjs'
+// const {add, subtract} = math
+
+
+// import 2 data atau lebih tanpa export default
+// import * as math from './math.mjs'
+// atau
+import {add, subtract} from './math.mjs'
+
+console.log(add(1,2))
+console.log(subtract(1,2))
+
